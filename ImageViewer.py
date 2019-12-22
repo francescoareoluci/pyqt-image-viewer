471e6ebb584e23b64f6fcf5e190f337ab5f87d7e
Francesco Areoluci <francesco.areoluci@magentalab.it>
2019-12-22 18:03:57 +0100

    Restored path handling in ImageViewer module

--- ImageViewer.py
+++ ImageViewer.py
@@ -6,7 +6,6 @@ This module is the entry point of the application.
 It will instantiates the QApplication, connect some signals
 and launch the application.
 """
-import os
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import qmlRegisterType, QQmlComponent, QQmlEngine, QQmlApplicationEngine
 from PyQt5.QtCore import QObject
@@ -28,9 +27,7 @@ if __name__ == '__main__':
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty('exifViewHandler', exifViewHandler)
     
-    qmlPath = '.' + os.sep + 'qml' + os.sep + 'ImageViewerWindow.qml'
-    #engine.load('./qml/ImageViewerWindow.qml')
-    engine.load(qmlPath)
+    engine.load('./qml/ImageViewerWindow.qml')
 
     # Get the root window
     win = engine.rootObjects()[0]
